38c7937dc63613f94481b1af2964665725ad7573
davitmartirosyan <phpencoding@gmail.com>
2024-06-25 03:13:22 +0400

    "x y positions got at any given time"

---
+++ libft/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 05:27:36 by dmartiro          #+#    #+#             */
+/*   Updated: 2024/05/10 01:08:50 by dmartiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+# define BUFFER_SIZE 1
+char	*get_next_line(int fd);
+#endif
